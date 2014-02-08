1f18aea4bb68fdca6232e6f993886ee5b9518fbf
Craig Boal <craig.boal@gmail.com>
2014-02-07 19:47:19 -0700

    Add delete functionality

--- js/views/todos.js
+++ js/views/todos.js
@@ -12,10 +12,29 @@ app.TodoView = Backbone.View.extend({
 
 	// Cache the template function for a single item.
 	template: _.template( $('#item-template').html() ),
+	
+	// NEW
+	events: {
+		'click .destroy': 'clear'          
+	},
 
+	
+	// NEW
+	// The TodoView listens for changes to its model, re-rendering. Since there's
+	// a one-to-one correspondence between a **Todo** and a **TodoView** in this
+	// app, we set a direct reference on the model for convenience.
+	initialize: function() {
+		this.listenTo(this.model, 'destroy', this.remove);
+	},
+	
 	// Re-render the titles of the todo item.
 	render: function() {
 		this.$el.html( this.template( this.model.toJSON() ) );
 		return this;
 	},
+	
+	// NEW - Remove the item, destroy the model from *localStorage* and delete its view.
+	clear: function() {
+		this.model.destroy();
+	}
 });
