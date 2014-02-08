7f16035fb1cd88ad121008f98eaed2f759e8f17b
Craig Boal <craig.boal@gmail.com>
2014-02-07 20:24:39 -0700

    Add functionality to mark todo entry as complete

--- js/models/todo.js
+++ js/models/todo.js
@@ -10,5 +10,13 @@ app.Todo = Backbone.Model.extend({
 	defaults: {
 		title: '',
 		completed: false
+	},
+	
+	// NEW
+	// Toggle the `completed` state of this todo item.
+	toggle: function() {
+		this.save({
+			completed: !this.get('completed')
+		});
 	}
 });
--- js/views/todos.js
+++ js/views/todos.js
@@ -13,27 +13,35 @@ app.TodoView = Backbone.View.extend({
 	// Cache the template function for a single item.
 	template: _.template( $('#item-template').html() ),
 	
-	// NEW
 	events: {
-		'click .destroy': 'clear'          
+		'click .destroy': 'clear',
+		// NEW
+		'click .toggle': 'togglecompleted' 		
 	},
 
-	
-	// NEW
 	// The TodoView listens for changes to its model, re-rendering. Since there's
 	// a one-to-one correspondence between a **Todo** and a **TodoView** in this
 	// app, we set a direct reference on the model for convenience.
 	initialize: function() {
+		//NEW
+		this.listenTo(this.model, 'change', this.render);
 		this.listenTo(this.model, 'destroy', this.remove);
 	},
 	
 	// Re-render the titles of the todo item.
 	render: function() {
 		this.$el.html( this.template( this.model.toJSON() ) );
+		//NEW
+		this.$el.toggleClass( 'completed', this.model.get('completed') ); // NEW
 		return this;
 	},
 	
-	// NEW - Remove the item, destroy the model from *localStorage* and delete its view.
+	// NEW - Toggle the `"completed"` state of the model.
+	togglecompleted: function() {
+		this.model.toggle();
+	},
+	
+	// Remove the item, destroy the model from *localStorage* and delete its view.
 	clear: function() {
 		this.model.destroy();
 	}
